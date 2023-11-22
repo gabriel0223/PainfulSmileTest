daa93edd2b24693357b01f8f644b9a1273c608c4
Gabriel Dias <gabriel.dias0223@gmail.com>
2023-11-22 18:17:38 -0300

    Implement tileset rules

---
+++ Assets/_Project/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerControls _playerControls;
+
+    public float AccelerationInput { get; private set; }
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        AccelerationInput = _playerControls.Player.Accelerate.ReadValue<float>();
+    }
+}
